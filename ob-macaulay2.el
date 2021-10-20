50ad08e0283441b9d9458b215382383d0f9a79a8
Doug Torrance <dtorrance@piedmont.edu>
2021-10-20 16:51:29 -0400

    strip leading whitespace from M2 output

--- ob-macaulay2.el
+++ ob-macaulay2.el
@@ -1,12 +1,13 @@
 (require 'ob)
 (require 'M2)
+(require 'subr-x)
 
 (defun org-babel-execute:M2 (body params)
   "Execute a block of Macaulay2 code with org-babel.
 This function is called by `org-babel-execute-src-block'"
   (let ((result-type (cdr (assq :result-type params))))
     (pcase result-type
-      (`output (org-babel-eval M2-exe body))
+      (`output (string-trim-left (org-babel-eval M2-exe body)))
       (`value "TODO"))))
 
 (provide 'ob-macaulay2)
