2c8e71cefffd96a9adc909a1fffe54da850e5f09
Doug Torrance <dtorrance@piedmont.edu>
2021-10-19 21:37:51 -0400

    re-indent everything

--- ob-macaulay2.el
+++ ob-macaulay2.el
@@ -2,13 +2,13 @@
 (require 'M2)
 
 (defun org-babel-execute:M2 (body params)
-    "Execute a block of Macaulay2 code with org-babel.
+  "Execute a block of Macaulay2 code with org-babel.
 This function is called by `org-babel-execute-src-block'"
-    (let ((temp-file (org-babel-temp-file "M2-" ".m2"))
-	   (result-type (cdr (assq :result-type params))))
-      (pcase result-type
-	(`output (with-temp-file temp-file (insert body))
-		 (org-babel-eval (concat M2-exe " --script " temp-file) ""))
-	(`value "TODO"))))
+  (let ((temp-file (org-babel-temp-file "M2-" ".m2"))
+	(result-type (cdr (assq :result-type params))))
+    (pcase result-type
+      (`output (with-temp-file temp-file (insert body))
+	       (org-babel-eval (concat M2-exe " --script " temp-file) ""))
+      (`value "TODO"))))
 
 (provide 'ob-macaulay2)
