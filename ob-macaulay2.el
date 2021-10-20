724d01ed44306f4ccc94ecc6e725a5900f099694
Doug Torrance <dtorrance@piedmont.edu>
2021-10-20 19:17:12 -0400

    call org-babel-process-params

--- ob-macaulay2.el
+++ ob-macaulay2.el
@@ -5,7 +5,8 @@
 (defun org-babel-execute:M2 (body params)
   "Execute a block of Macaulay2 code with org-babel.
 This function is called by `org-babel-execute-src-block'"
-  (let ((result-type (cdr (assq :result-type params))))
+  (let* ((processed-params (org-babel-process-params params))
+	 (result-type (cdr (assq :result-type processed-params))))
     (pcase result-type
       (`output (string-trim-left (org-babel-eval M2-exe body)))
       (`value "TODO"))))
