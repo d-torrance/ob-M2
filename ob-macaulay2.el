942eb24c912283e5a4af0368e9763b76d10c3009
Doug Torrance <dtorrance@piedmont.edu>
2021-10-20 07:18:17 -0400

    Stop using temp file and let org-babel-eval do its thing

--- ob-macaulay2.el
+++ ob-macaulay2.el
@@ -4,11 +4,9 @@
 (defun org-babel-execute:M2 (body params)
   "Execute a block of Macaulay2 code with org-babel.
 This function is called by `org-babel-execute-src-block'"
-  (let ((temp-file (org-babel-temp-file "M2-" ".m2"))
-	(result-type (cdr (assq :result-type params))))
+  (let ((result-type (cdr (assq :result-type params))))
     (pcase result-type
-      (`output (with-temp-file temp-file (insert body))
-	       (org-babel-eval (concat M2-exe " --script " temp-file) ""))
+      (`output (org-babel-eval M2-exe body))
       (`value "TODO"))))
 
 (provide 'ob-macaulay2)
