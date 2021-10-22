51b2417883b91478b1ab34121e251f60f6cbc85a
Doug Torrance <dtorrance@piedmont.edu>
2021-10-21 22:19:29 -0400

    sessions (kind of) work!

--- ob-macaulay2.el
+++ ob-macaulay2.el
@@ -2,21 +2,31 @@
 (require 'M2)
 (require 'subr-x)
 
+(defconst org-babel-macaulay2-eoe-output "org_babel_macaulay2_eoe")
+(defconst org-babel-macaulay2-eoe-indicator
+  (prin1-to-string org-babel-macaulay2-eoe-output))
+
 (defun org-babel-macaulay2-initiate-session (&optional session)
   "If there is not a current inferior-process-buffer in SESSION then create.
 Return the initialized session."
-  (unless (string= session "none")
-    (buffer-name (M2 M2-exe session))))
+  (if (string= session "none") "none"
+    (buffer-name (save-excursion (M2 M2-exe session)))))
 
 (defun org-babel-execute:M2 (body params)
   "Execute a block of Macaulay2 code with org-babel.
 This function is called by `org-babel-execute-src-block'"
   (let* ((processed-params (org-babel-process-params params))
 	 (session (org-babel-macaulay2-initiate-session
-                     (cdr (assq :session processed-params))))
+                   (cdr (assq :session processed-params))))
 	 (result-type (cdr (assq :result-type processed-params))))
     (pcase result-type
-      (`output (string-trim-left (org-babel-eval M2-exe body)))
+      (`output (if (string= session "none")
+		   (string-trim-left (org-babel-eval M2-exe body))
+		 (org-babel-comint-with-output
+		     (session org-babel-macaulay2-eoe-output)
+		   (insert (concat body "\n"
+				   org-babel-macaulay2-eoe-indicator))
+		   (M2-send-to-program-or-jump-to-source-code))))
       (`value "TODO"))))
 
 (provide 'ob-macaulay2)
