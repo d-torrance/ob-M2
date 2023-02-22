90ca57d961251efbfc812ab96ed2c482b2efd79a
Doug Torrance <dtorrance@piedmont.edu>
2023-02-21 20:54:13 -0500

    Use "ob-macaulay2-*" as prefix

--- ob-macaulay2.el
+++ ob-macaulay2.el
@@ -19,36 +19,36 @@
 
 (add-to-list 'org-babel-tangle-lang-exts '("macaulay2" . "m2"))
 
-(defconst org-babel-macaulay2-eoe-output "org_babel_macaulay2_eoe"
+(defconst ob-macaulay2-eoe-output "org_babel_macaulay2_eoe"
   "String to indicate that Macaulay2 output has completed.")
 
-(defconst org-babel-macaulay2-eoe-indicator
+(defconst ob-macaulay2-eoe-indicator
   (concat "print "
-	  (prin1-to-string org-babel-macaulay2-eoe-output))
+	  (prin1-to-string ob-macaulay2-eoe-output))
   "Command to print string to indicate that Macaulay2 output has completed")
 
-(defconst org-babel-macaulay2-command
+(defconst ob-macaulay2-command
   (concat M2-exe " --no-prompts --silent -e 'clearEcho stdio'")
   "Name of the command for executing Macaulay2 code.")
 
-(defun org-babel-macaulay2-initiate-session (session)
+(defun ob-macaulay2-initiate-session (session)
   "If there is not a current inferior-process-buffer in SESSION then create.
 Return the initialized session."
   (if (string= session "none") "none"
     (buffer-name
-     (save-window-excursion (M2 org-babel-macaulay2-command session)))))
+     (save-window-excursion (M2 ob-macaulay2-command session)))))
 
-(defconst org-babel-macaulay2-value-output "org_babel_macaulay2_value")
+(defconst ob-macaulay2-value-output "org_babel_macaulay2_value")
 
-(defun org-babel-macaulay2-prepare-value (body)
+(defun ob-macaulay2-prepare-value (body)
   (concat body "\n"
-	  "print " (prin1-to-string org-babel-macaulay2-value-output) "\n"
+	  "print " (prin1-to-string ob-macaulay2-value-output) "\n"
 	  "print oo"))
 
-(defun org-babel-macaulay2-get-value (output)
-  (car (last (split-string output org-babel-macaulay2-value-output))))
+(defun ob-macaulay2-get-value (output)
+  (car (last (split-string output ob-macaulay2-value-output))))
 
-(defun org-babel-macaulay2-evaluate-session (session body result-type)
+(defun ob-macaulay2-evaluate-session (session body result-type)
   "Pass BODY to the Macaulay2 process in SESSION.
 If RESULT-TYPE equals `output' then return standard output as a
 string.  If RESULT-TYPE equals `value' then return the value of the
@@ -58,11 +58,11 @@ last statement in BODY, as elisp."
 	(prepare-body
 	 (pcase result-type
 	   (`output 'identity)
-	   (`value 'org-babel-macaulay2-prepare-value)))
+	   (`value 'ob-macaulay2-prepare-value)))
 	(process-output
 	 (pcase result-type
 	   (`output 'identity)
-	   (`value 'org-babel-macaulay2-get-value))))
+	   (`value 'ob-macaulay2-get-value))))
     (with-current-buffer session
       (setq-local comint-prompt-regexp "^"))
     (prog1
@@ -73,21 +73,21 @@ last statement in BODY, as elisp."
 		   (seq-remove
 		    (lambda (line)
 		      (or
-		       (string-match-p org-babel-macaulay2-eoe-output
+		       (string-match-p ob-macaulay2-eoe-output
 				       line)
 		       (string-match-p "^+ M2" line)))
 		    (org-babel-comint-with-output
-			(session org-babel-macaulay2-eoe-output)
+			(session ob-macaulay2-eoe-output)
 		      (insert
 		       (concat (funcall prepare-body body) "\n"
-			       org-babel-macaulay2-eoe-indicator))
+			       ob-macaulay2-eoe-indicator))
 		      (comint-send-input nil t)))))
 	 "[\n\r]+")
       (with-current-buffer session
 	(setq-local comint-prompt-regexp
 		    comint-prompt-regexp-old)))))
 
-(defun org-babel-macaulay2-evaluate-external-process (body result-type)
+(defun ob-macaulay2-evaluate-external-process (body result-type)
   "Evaluate BODY in external Macaulay2 process.
 If RESULT-TYPE equals `output' then return standard output as a
 string.  If RESULT-TYPE equals `value' then return the value of the
@@ -95,14 +95,14 @@ last statement in BODY, as elisp."
   (let ((prepare-body
 	 (pcase result-type
 	   (`output 'identity)
-	   (`value 'org-babel-macaulay2-prepare-value)))
+	   (`value 'ob-macaulay2-prepare-value)))
 	(process-output
 	 (pcase result-type
 	   (`output 'identity)
-	   (`value 'org-babel-macaulay2-get-value))))
+	   (`value 'ob-macaulay2-get-value))))
     (string-trim-left
      (funcall process-output
-	      (org-babel-eval org-babel-macaulay2-command
+	      (org-babel-eval ob-macaulay2-command
 			      (funcall prepare-body body)))
      "[\n\r]+")))
 
@@ -116,16 +116,16 @@ last statement in BODY, as elisp."
   "Execute a block of Macaulay2 code with org-babel.
 This function is called by `org-babel-execute-src-block'"
   (let* ((processed-params (org-babel-process-params params))
-	 (session (org-babel-macaulay2-initiate-session
+	 (session (ob-macaulay2-initiate-session
                    (cdr (assq :session processed-params))))
 	 (result-type (cdr (assq :result-type processed-params)))
 	 (full-body (org-babel-expand-body:generic
 		     body params
 		     (org-babel-variable-assignments:M2 params))))
     (if (string= session "none")
-	(org-babel-macaulay2-evaluate-external-process
+	(ob-macaulay2-evaluate-external-process
 	 full-body result-type)
-      (org-babel-macaulay2-evaluate-session
+      (ob-macaulay2-evaluate-session
        session full-body result-type))))
 
 (provide 'ob-macaulay2)
