d0ff2bce5bd6b5918ebf3d965135f074d7869260
Doug Torrance <dtorrance@piedmont.edu>
2023-03-04 21:11:59 -0500

    Require Emacs 27.1 for tests (for org-show-all)

--- test-ob-M2.el
+++ test-ob-M2.el
@@ -2,6 +2,7 @@
 
 ;; Copyright (C) 2021-2023 Doug Torrance
 
+;; Package-Requires: ((emacs "27.1"))
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
